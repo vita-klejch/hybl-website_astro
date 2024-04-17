b2400b59f627cef2f6ed35546449f68191328436
vita-klejch <vita9@post.cz>
2024-04-17 22:36:40 +0200

    some updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+// const mode = import.meta.env.MODE;
+// console.log("msg from CONFIG");
+const base = import.meta.env.BASE_URL;
+// console.log(import.meta.url);
+// console.log("base", base);
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://vita-klejch.github.io",
+  // base: "hybl-website_astro",
+  base: base,
+});
